23817d3936d3a15e96083d015508af927d6f170a
Shriram33 <131244637+Shriram33@users.noreply.github.com>
2023-09-24 14:31:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,34 +14,6 @@ data "aws_ami" "fetch_ami" {
   }
 }
 
-# Creating the Ansible Remote machine #
-
-resource "aws_instance" "remote" {
-  ami = data.aws_ami.fetch_ami.id
-  instance_type = local.insta
-  count = 2
-  tags = {
-    name = "Ansible_Remote_Node.${count.index}"
-  }
-
-  connection {
-    type = "ssh"
-    user = "ec2-user"
-    host = self.public_ip
-  }
-
-  provisioner "remote-exec" {
-    on_failure = "continue"
-    inline = [ 
-      "sudo install -y python"
-     ]
-  }
-}
-
-output "ip_of_remote_node" {
-  value = aws_instance.remote[*].public_ip
-}
-
 # Creating the Ansible Control machine #
 
 resource "aws_instance" "control" {
@@ -49,22 +21,8 @@ resource "aws_instance" "control" {
   instance_type = local.insta
   tags = {
     name = "Ansible_Control_Node"
-  }
-
-  connection {
-    type = "ssh"
-    user = "ec2-user"
-    host = self.public_ip
-  }
-
-  provisioner "remote-exec" {
-    on_failure = "continue"
-    inline = [ 
-      "sudo amazon-linux-extras install -y ansible2"
-     ]
-  }
 }
-
+ 
 locals {
   insta = "t2.micro"
 }
